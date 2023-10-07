367d33253f773543fbcf5e7dfc4d295ddfce97ef
Amanda <amandasinggih17@gmail.com>
2023-10-07 11:09:28 +0700

    adding the file

---
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Guide for user to use our scientific calculator
+    cout << "==== Welcome to our Scientific Calculator! ====" << endl;
+    cout << "What function do you want to use?" << endl;
+    cout << "Press 1 for addition" << endl;
+    cout << "Press 2 for substraction" << endl;
+    cout << "Press 3 for multiplication" << endl;
+    cout << "Press 4 for division" << endl;
+    cout << "Press 5 for trigonometry function" << endl;
+    cout << "Press 6 for ";
+    return 0;
+}
