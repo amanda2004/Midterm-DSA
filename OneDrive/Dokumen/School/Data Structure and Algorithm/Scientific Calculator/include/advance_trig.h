40b9aa29f0658de14734baedee4ba201c07bb4bd
MHafiz12345 <147223828+MHafiz12345@users.noreply.github.com>
2023-10-08 17:03:48 +0700

    Update advance_trig.h with comment

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/advance_trig.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/advance_trig.h
@@ -1,20 +1,23 @@
-#ifndef ADVANCE_TRIG_H
-#define ADVANCE_TRIG_H
+// advance_trig.h
+#ifndef ADVANCE_TRIG_H // This line checks if ADVANCE_TRIG_H has not been defined yet
+#define ADVANCE_TRIG_H // This line defines ADVANCE_TRIG_H
 
-
+// This is the declaration of the advance_trig class
 class advance_trig
 {
-    public:
-        char selected;
+    public: // These members are accessible from outside the class
+        int selected;  // This is a public member variable. It's an integer that stores the selected option
 
+        // This is the constructor for the advance_trig class. It initializes selected to 0
         advance_trig() {
-            selected = '0';
+            selected = 0;
         }
 
-        void start();
-        void arccos();
-        void arcsin();
-        void arctan();
+        // These are declarations for public member functions of the advance_trig class
+        void start(); // This function will start the program
+        void arccos(); // This function will calculate the arccosine
+        void arcsin(); // This function will calculate the arcsine
+        void arctan(); // This function will calculate the arctangent
 };
 
-#endif // ADVANCE_TRIG_H
+#endif // This line ends the conditional directive from the #ifndef at the top
