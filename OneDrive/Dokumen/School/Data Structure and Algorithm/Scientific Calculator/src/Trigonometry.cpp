281e5288b69526d73cdcdf5ebd13a71d9b387b83
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-09 01:49:46 +0700

    Update Trigonometry.cpp

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/Trigonometry.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/Trigonometry.cpp
@@ -1,13 +1,16 @@
 #include "Trigonometry.h"
+#include "History.h"
+#include <string>
 #include <cmath>
 #include <iostream>
 #define PI 3.141592654
 
 using namespace std;
 
-void Trigonometry::start()
+void Trigonometry::start(history_list& history)
 {
     do {
+        cout << "Press 0 for back to main calculator" << endl;
         cout << "Press 1 for Sin" << endl;
         cout << "Press 2 for Cos" << endl;
         cout << "Press 3 for Tan" << endl;
@@ -16,15 +19,15 @@ void Trigonometry::start()
         switch (selected)
         {
         case '1':
-            calculateSine();
+            calculateSine(history);
             break;
 
         case '2':
-            calculateCosine();
+            calculateCosine(history);
             break;
 
         case '3':
-            calculateTangent();
+            calculateTangent(history);
             break;
 
         case '0':
@@ -37,37 +40,46 @@ void Trigonometry::start()
     } while (selected != '0');
 }
 
-void Trigonometry::calculateSine()
+void Trigonometry::calculateSine(history_list& history)
 {
     double x, result;
-        cout << "Enter number (in degrees):";
-        cin >> x;
+    cout << "Enter number (in degrees):";
+    cin >> x;
 
     result = sin(x * PI / 180.0);
-    cout << "Sin(x) = " << result << " radians" << endl;
-    cout << "Sin(x) = " << result * 180 / PI << " degrees" << endl << endl;
+    cout << "Sin(" << x << ") = " << result << endl;
+
+    //to store the result in the calculator's history
+    string output = "Sin(" + to_string(x) + ") = " + to_string(result);
+    history.add_result(output);
 }
 
-void Trigonometry::calculateCosine()
+void Trigonometry::calculateCosine(history_list& history)
 {
     double x, result;
-        cout << "Enter number (in degrees):";
-        cin >> x;
+    cout << "Enter number (in degrees):";
+    cin >> x;
 
     result = cos(x * PI / 180.0);
-    cout << "Cos(x) = " << result << " radians" << endl;
-    cout << "Cos(x) = " << result * 180 / PI << " degrees" << endl << endl;
+    cout << "Cos(" << x << ") = " << result << endl;
+
+    //to store the result in the calculator's history
+    string output = "Cos(" + to_string(x) + ") = " + to_string(result);
+    history.add_result(output);
 }
 
-void Trigonometry::calculateTangent()
+void Trigonometry::calculateTangent(history_list& history)
 {
     double x, result;
-        cout << "Enter number (in degrees):";
-        cin >> x;
+    cout << "Enter number (in degrees):";
+    cin >> x;
 
     result = tan(x * PI / 180.0);
-    cout << "Tan(x) = " << result << "radians" << endl;
-    cout << "Tan(x) = " << result * 180 / PI << "degrees" << endl << endl;
+    cout << "Tan(" << x << ") = " << result << endl;
+
+    //to store the result in the calculator's history
+    string output = "Tan(" + to_string(x) + ") = " + to_string(result);
+    history.add_result(output);
 }
 
 
