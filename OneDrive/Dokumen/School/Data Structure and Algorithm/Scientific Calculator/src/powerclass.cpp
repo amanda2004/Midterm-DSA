baf5b79eaaa78f31cedc7ebf6cde974feb2ebab7
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-09 00:47:50 +0700

    Update powerclass.cpp

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/powerclass.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/powerclass.cpp
@@ -4,13 +4,12 @@
 #include <string>
 #include "History.h"
 #include "powerclass.h"
-#define PI 3.141592654
 
 void power::start(history_list& history)
 {
     using namespace std;
 
-    int base, exponent, result;
+    double base, exponent, result;
 
     cout << "Enter the base number: ";
     cin >> base;
@@ -20,6 +19,7 @@ void power::start(history_list& history)
     result = pow(base, exponent);
     cout << base << " ^ " << exponent << " = " << result << endl << endl;
 
+    //to store the result in the calculator's history
     string output = to_string(base) + " ^ " + to_string(exponent) + " = " + to_string(result);
     history.add_result(output);
 }
