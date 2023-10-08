33d643d15dc1b4042c2328978adc521969a9667e
MHafiz12345 <147223828+MHafiz12345@users.noreply.github.com>
2023-10-08 20:13:44 +0700

    Update History.cpp new history testing 1.1

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/History.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/History.cpp
@@ -1,19 +1,14 @@
-//These codes managing a history of calculator results
-
-//Include necessary headers and libraries
 #include "History.h"
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
-//Constructor for the history_list class
 history_list::history_list(): head(nullptr), tail(nullptr){}
 
-//Destructor for the history_list class
 history_list::~history_list()
 {
-    //Delete all nodes in the linked list to free memory
     while(head){
         Node* temp = head;
         head = head -> next;
@@ -21,41 +16,27 @@ history_list::~history_list()
     }
 }
 
-//Function to add a new result to the history list
-void history_list::add_result(double result)
-{
-    //Create a new node with the given result
+void history_list::add_result(string result){
     Node* new_node = new Node(result);
-
-    //If the list is empty, set both head and tail to the new node
     if(!head){
         head = new_node;
         tail = new_node;
     } else {
-        //Otherwise, append the new node to the end of the list and update
         tail -> next = new_node;
         tail = new_node;
     }
 }
 
-//Function to display the calculator's history
-void history_list::show_history()
-{
-    //Check if the history list is empty
+void history_list::show_history(){
     if(!head){
-        std::cout << "Calculator History is not available." << std::endl;
+        cout << "Calculator History is not available." << endl;
         return;
     }
 
-    //Initialize a pointer to traverse the linked list starting from the head
     Node* current = head;
-
-    //Display a message indicating the start of the history list
-    std::cout << "Calculator History List:" << std::endl;
-
-    //Traverse the linked list and print each result
+    cout << "Calculator History List:" << endl;
     while(current){
-        std::cout << current -> data << std::endl;
+        cout << current -> data << endl;
         current = current -> next;
     }
 }
