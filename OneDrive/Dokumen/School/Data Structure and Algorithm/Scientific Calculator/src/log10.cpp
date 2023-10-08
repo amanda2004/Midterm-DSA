a42ee277aa8646d58c690c4b18eabcdde1490d3a
rxbbons <145674959+rxbbons@users.noreply.github.com>
2023-10-08 16:48:55 +0700

    Update log10.cpp
    Added information regarding the code

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/log10.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/log10.cpp
@@ -6,10 +6,15 @@
 
     void logarithms10::start()
     {
+    // To declare the number that the user s going to input as a variable
     double log10num;
+    // Asking the user to input the number
     std::cout << "Enter a number: ";
+    // The number inputted will be stored in log10num
     std::cin >> log10num;
 
+    // Declaring that the variable log10val is the result of log(log10num)
     double log10val = log10  (log10num);
+    // Showing the result to the user
     std::cout << "The value of " << log10num << " to the base of 10 is " << log10val << "\n";
     }
