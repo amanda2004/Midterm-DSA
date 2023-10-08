c701f618f70e20657993522896ab2fc7b50b537b
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-09 01:03:45 +0700

    Update LogCustomBase.cpp

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/LogCustomBase.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/LogCustomBase.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include "LogCustomBase.h"
+#include "History.h"
+#include <string>
 #include <cmath>
 #include <cstdlib>
+
 using namespace std;
 
-void LogCustomBase::start() 
+void LogCustomBase::start(history_list& history) 
 {
+    double base, value;
 
-  double base, value;
+    cout << "Enter base number: ";
+    cin >> base;
+    cout << "Enter number: ";
+    cin >> value;
+      
+    double logcustom = log(value) / log(base);
+    cout << "The value of " << value << " with the base of " << base << " is " << logcustom;
 
-  cout << "Enter base number: ";
-  cin >> base;
-  cout << "Enter number: ";
-  cin >> value;
-  
-  double logcustom = log(value) / log(base);
-  cout << "The value of " << value << " with the base of " << base << " is " << logcustom;
-
+    //to store the result in the calculator's history
+    string output = "The value of " + to_string(value) + " with the base of " + to_string(base) + " is " + to_string(logcustom);
+    history.add_result(output);
 }
