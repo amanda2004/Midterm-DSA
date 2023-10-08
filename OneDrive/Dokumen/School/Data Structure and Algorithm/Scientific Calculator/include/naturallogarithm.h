a0cc4b865caac3249213ad969a93961e5ea9b9ed
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-09 01:10:48 +0700

    Update naturallogarithm.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/naturallogarithm.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/naturallogarithm.h
@@ -1,11 +1,11 @@
 #ifndef NATURALLOGARITHM_H
 #define NATURALLOGARITHM_H
-
+#include "History.h"
 
 class naturallogarithm
 {
     public:
-        void start ();
+        void start (history_list& history);
 };
 
 #endif // NATURALLOGARITHM_H
