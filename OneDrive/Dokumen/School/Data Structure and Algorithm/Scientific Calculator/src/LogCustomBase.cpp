3f9b503616424887ba87030230a47415bf8c4c7b
rxbbons <145674959+rxbbons@users.noreply.github.com>
2023-10-08 19:03:45 +0700

    Update LogCustomBase.cpp

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/LogCustomBase.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/LogCustomBase.cpp
@@ -7,13 +7,16 @@ using namespace std;
 void LogCustomBase::start() 
 {
 
+  // Declaring the base and value as double
   double base, value;
 
+  // Asking the user to input both base of the logarithm and the value that will be stored to base & value
   cout << "Enter base number: ";
   cin >> base;
   cout << "Enter number: ";
   cin >> value;
-  
+
+  // Declaring logcustom as the result of the user input
   double logcustom = log(value) / log(base);
   cout << "The value of " << value << " with the base of " << base << " is " << logcustom;
 
