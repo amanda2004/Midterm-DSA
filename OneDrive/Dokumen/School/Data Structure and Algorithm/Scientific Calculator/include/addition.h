76277b9cba2c04730ec9d4341f29f90099cffd1b
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-09 16:30:51 +0700

    Update addition.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/addition.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/addition.h
@@ -1,13 +1,19 @@
+//Check if ADDITION_H is already defined or not to prevent double inclusion
 #ifndef ADDITION_H
 #define ADDITION_H
+
+//Include the "History.h" header file, which is needed for this class
 #include "History.h"
 
-
+//Declaration of the addition class
 class addition
 {
     public:
+        //Public method declaration: start
+        //This method takes a reference to a history_list object as an argument
         void start(history_list& history);
 
 };
 
+//End of the header file guard
 #endif // ADDITION_H
