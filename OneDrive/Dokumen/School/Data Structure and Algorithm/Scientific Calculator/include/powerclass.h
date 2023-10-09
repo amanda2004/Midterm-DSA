058a32a8118944d3f93fde1cff69eb54a6c0edf7
MHafiz12345 <147223828+MHafiz12345@users.noreply.github.com>
2023-10-09 16:01:16 +0700

    Update powerclass.h with comment

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/powerclass.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/powerclass.h
@@ -1,12 +1,13 @@
-#ifndef POWER_H
-#define POWER_H
-#include "History.h"
+// This is a header guard. It prevents the header file from being included more than once in a single compilation.
+#ifndef POWER_H // This line checks if POWER_H has not been defined yet
+#define POWER_H // This line defiNES POWER_H
+#include "History.h" // This includes the "History.h" header file, which contains the declaration of the "history_list" class.
 
-class power
+class power // This is the declaration of the "power" class.
 {
-    public:
-        void start(history_list& history);
+    public: // These members are accessible from outside the class
+        void start(history_list& history); // This is a public member function named "start". It takes a reference to an object of type "history_list" as its argument.
 };
 
 
-#endif
+#endif // This line ends the conditional directive from the #ifndef at the top
