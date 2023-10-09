b9df456d2e39dde8afd4d8d8870aeb377fb60e24
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-09 16:35:30 +0700

    Update Division.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/Division.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/Division.h
@@ -1,13 +1,19 @@
+//Check if DIVISION_H is already defined or not to prevent double inclusion
 #ifndef DIVISION_H
 #define DIVISION_H
+
+//Include the "History.h" header file, which is needed for this class
 #include "History.h"
 
-
+//Declaration of the division class
 class division
 {
     public:
+        //Public method declaration: start
+        //This method takes a reference to a history_list object as an argument
         void start(history_list& history);
 
 };
 
+//End of the header file guard
 #endif // DIVISION_H
