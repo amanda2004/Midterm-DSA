59511173b047b14760a5b6037f55f3903b36923b
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 16:42:35 +0700

    Update log10.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/log10.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/log10.h
@@ -1,10 +1,13 @@
 #ifndef LOG10_H
 #define LOG10_H
-#include "History.h"
+#include "History.h" //Include the declaration of the 'history_list' class
 
+//Declaration of 'logarithms10' class
 class logarithms10
 {
     public:
+        //Declaration of a public member function named 'start'
+        //It takes one argument, which is a reference to an object of type 'history_list'
         void start (history_list& history);
 
 };
