ace447855cec80350290647c6cd2d00d9070b5a7
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 16:45:53 +0700

    Update multiplication.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/multiplication.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/multiplication.h
@@ -1,11 +1,13 @@
 #ifndef multiplication_H
 #define multiplication_H
-#include "History.h"
+#include "History.h" //Include the 'History.h' header file for dependencies
 
-
+//Declaration of 'multiplication' class
 class multiplication
 {
     public:
+        //Declaration of a public member function named 'start'
+        //It takes one argument, which is a reference to an object type 'history_list'
         void start(history_list& history);
 
 };
