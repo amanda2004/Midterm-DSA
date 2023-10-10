22fc09db4b80a7466a6a48487606543dd0c5fc71
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 16:52:17 +0700

    Update multiplication.cpp

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/multiplication.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/multiplication.cpp
@@ -1,13 +1,13 @@
-#include "multiplication.h"
-#include "History.h"
+#include "multiplication.h" //Include the header file for the 'multiplication' class
+#include "History.h" //Include the header file for the 'history_list' class
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 
+//Define the 'start' function of the 'multiplication' class
 void multiplication ::start(history_list& history)
-{
-
+{    
 using namespace std;
 
     double multiply;
@@ -22,16 +22,18 @@ using namespace std;
 
     string output = to_string(n); // Start the output string with the first number
 
+    //Loop to read and multiply additional numbers
     for(int i=2; i<=numberitems; i++)
     {
         cout << "Enter number " << i << ": " ;
         cin >> n;
         multiply *= n;
-        output += " x " + to_string(n); // Add to the output string
+        output += " x " + to_string(n); // Add the number and multiplication symbol to the output string
     }
 
     output += " = " + to_string(multiply); // Add the result to the output string
-    cout << output << endl << endl;
+    cout << output << endl << endl; //Display the result
 
+    //Add the result string to the 'history' object
     history.add_result(output);
 }
