3e677915b622478f4d026b331ba199c7ca473fd8
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 16:30:18 +0700

    Update LogCustomBase.cpp
    add comment

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/LogCustomBase.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/LogCustomBase.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
-#include "LogCustomBase.h"
-#include "History.h"
+#include "LogCustomBase.h" //Include the declaration of the LogCustomBase class
+#include "History.h" //Include the declaration of the history_list class
 #include <string>
 #include <cmath>
 #include <cstdlib>
 
 using namespace std;
 
+//Define the start function of the LogCustomBase class, which calculates logarithms with a custom base.
 void LogCustomBase::start(history_list& history) 
 {
-    double base, value;
+    double base, value; //Declare variables to store the base and value
 
     cout << "Enter base number: ";
-    cin >> base;
+    cin >> base; //Prompt the user to enter the base number
     cout << "Enter number: ";
-    cin >> value;
-      
+    cin >> value; //Prompt the user to enter the number for which the logarithms will be calculated
+
+    //Calculate the logarithm with the custom base using the formula log(value) / log(base)
     double logcustom = log(value) / log(base);
+
+    //Display the result to the user
     cout << "The value of " << value << " with the base of " << base << " is " << logcustom << "\n" << "\n";
 
     //to store the result in the calculator's history
