98f5244fbb654162abdc3d8d0a85539c48ff0cb2
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 17:00:36 +0700

    Update substraction.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/substraction.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/substraction.h
@@ -1,10 +1,13 @@
 #ifndef SUBSTRACTION_H
 #define SUBSTRACTION_H
-#include "History.h"
+#include "History.h" //Include "History.h" header file for dependencies
 
+//Defines 'substraction' class
 class substraction
 {
     public:
+        //Declaration of one public function member named 'start'
+        //It takes one argument that reference to object type 'history_list'
         void start(history_list& history);
 
 };
