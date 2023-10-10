1138d7e421b9b631ea149d91e905b1727b9e4c17
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 16:55:41 +0700

    Update naturallogarithm.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/naturallogarithm.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/naturallogarithm.h
@@ -1,10 +1,13 @@
 #ifndef NATURALLOGARITHM_H
 #define NATURALLOGARITHM_H
-#include "History.h"
+#include "History.h" //Include the "History.h" header file for dependencies
 
+//This is the declaration of 'naturallogarithm' class
 class naturallogarithm
 {
     public:
+        //This is the declaration of a public member function named 'start'
+        //It takes one argument, which is a reference to an object of type 'history_list'
         void start (history_list& history);
 };
 
