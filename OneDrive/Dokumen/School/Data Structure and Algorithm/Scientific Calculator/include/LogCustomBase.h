3c2f00d98a57a5e44c7eb33bdc52911209209a5d
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 16:34:29 +0700

    Update LogCustomBase.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/LogCustomBase.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/LogCustomBase.h
@@ -1,10 +1,12 @@
 #ifndef LOGCUSTOMBASE_H
 #define LOGCUSTOMBASE_H
-#include "History.h"
+#include "History.h" //Include the declaration of the 'history_list' class from "History.h"
 
+//Declare the 'LogCustomBase' class
 class LogCustomBase
 {
     public:
+        //Declare a public member function named 'start' that takes a reference to a 'history' object
         void start(history_list& history);
 };
 
