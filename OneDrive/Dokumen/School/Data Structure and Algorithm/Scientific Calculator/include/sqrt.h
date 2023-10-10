24957167a0acf002494db5efced25a59c32d59c5
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 16:58:14 +0700

    Update sqrt.h

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/sqrt.h
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/include/sqrt.h
@@ -1,10 +1,13 @@
 #ifndef SQRT_H
 #define SQRT_H
-#include "History.h"
+#include "History.h" //Include "History.h" header file for dependencies
 
+//Defines 'sqrtCalculator' class
 class sqrtCalculator
 {
 public:
+    //Declaration of a public member function named 'start'
+    //It takes one argument that reference to the object type 'history_list'
     void start (history_list& history);
 };
 
