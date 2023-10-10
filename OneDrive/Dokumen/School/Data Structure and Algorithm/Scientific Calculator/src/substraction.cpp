9e5bcca821cee67afd1d7c961837f39b9e5507f1
amanda2004 <78061764+amanda2004@users.noreply.github.com>
2023-10-10 17:05:57 +0700

    Update substraction.cpp

--- OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/substraction.cpp
+++ OneDrive/Dokumen/School/Data Structure and Algorithm/Scientific Calculator/src/substraction.cpp
@@ -1,10 +1,11 @@
-#include "substraction.h"
-#include "History.h"
+#include "substraction.h" //Include the header file for 'substraction' class
+#include "History.h" //Include the header file for 'history_list' class
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 
+//Define the 'start' function of the 'substraction' class
 void substraction::start(history_list& history)
 {
 
@@ -22,16 +23,18 @@ using namespace std;
 
     string output = to_string(n); // Start the output string with the first number
 
+    //Loop to read and subtract additional numbers
     for(int i=2; i<=numberitems; i++)
     {
         cout << "Enter number " << i << ": " ;
         cin >> n;
         sub -= n;
-        output += " - " + to_string(n); // Add to the output string
+        output += " - " + to_string(n); // Add the number and subtraction symbol to the output string
     }
 
     output += " = " + to_string(sub); // Add the result to the output string
-    cout << output << endl << endl;
+    cout << output << endl << endl; //Display the result
 
+    //Add the result string to the calculator's history
     history.add_result(output);
 }
